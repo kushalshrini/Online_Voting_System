1116e2c9a526532bc775447853b22a2e60964315
kushalshrini <42143431+kushalshrini@users.noreply.github.com>
2021-04-11 14:01:55 +0530

    Add files via upload

--- test/controllers/voter_controller_test.rb
+++ test/controllers/voter_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class AdminControllerTest < ActionDispatch::IntegrationTest
+class VoterControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
